036791b182c852a3d5d3546af62dd123dd6aa90d
omk18p <omkar003628p@gmail.com>
2025-07-11 23:47:42 +0530

    ,

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,6 @@
 import axios from 'axios'
 
-export const axiosi=axios.create({withCredentials:true,baseURL:process.env.REACT_APP_BASE_URL})
+export const axiosi = axios.create({
+  withCredentials: true,
+  baseURL: process.env.REACT_APP_BASE_URL || 'https://e-commerce-5rkc.onrender.com'
+})
